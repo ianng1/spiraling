4525c16633f484e014398290493f4a13d79216f6
Liu <48111348+ALBHL@users.noreply.github.com>
2024-05-14 14:43:08 -0700

    poster

--- Scripts/chest.gd
+++ Scripts/chest.gd
@@ -8,9 +8,11 @@ var is_mouse_on_chest: bool = false
 var is_interface_open: bool = false
 var is_opened: bool = false
 var code_entered: int = 0
+var is_poster_open: bool = false
 
 @onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 @onready var tween = get_tree().create_tween()
+@onready var poster = %poster
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,13 +31,16 @@ func _open_box():
 	is_interface_open = false
 	animation_player.play("open")
 	print("chest was opened successfully")
+	if poster:
+		poster.visible = true
+		is_poster_open = true
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var number_path = "/root/Level_01/UserInterface/GameUI/chest_numberPad/Lockbox/NumberPad"
 	if get_node(number_path):
 		code_entered = get_node(number_path).current_code
-	if code_entered == 0617 and is_interface_open:
+	if code_entered == 0111 and is_interface_open:
 		_open_box()
 
 func _mouse_on_box():
@@ -67,5 +72,8 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and is_interface_open:
 		%chest_numberPad.visible = false
 		is_interface_open = false
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and is_poster_open:
+		%poster.visible = false
+		is_poster_open = false
 
 
