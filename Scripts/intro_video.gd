af760a882a65abbeaaa6a39f89635d73be714183
Liu <48111348+ALBHL@users.noreply.github.com>
2024-05-28 15:37:59 -0700

    add skip button

--- Scripts/intro_video.gd
+++ Scripts/intro_video.gd
@@ -13,3 +13,7 @@ func _process(delta):
 
 func _on_video_stream_player_finished():
 	get_tree().change_scene_to_file("res://Scenes/Levels/Level_01.tscn")
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/Level_01.tscn")
